76960d52f5aef8aae9214663f468523dc37695bd
yoonsang <highp0912@pm.me>
2021-05-27 18:11:17 +0900

    Doc: Add comments in template-injector.ts

--- src/client-app/template/template-injector.ts
+++ src/client-app/template/template-injector.ts
@@ -1,27 +1,52 @@
-export class TemplateInjector<TCreateEl extends HTMLElement> {
+export class TemplateInjector<TCreateEl extends HTMLElement> {  
   private templateEl: HTMLTemplateElement;
+
+  /**
+   * rool element of this created element
+   */
   private rootEl: HTMLDivElement;
+
   private createdEl: TCreateEl;
+  /**
+   * if not exists, it's falsy null
+   */
+  public get getCreatedEl(): TCreateEl {
+    return this.createdEl;
+  }
+
+  private curElId: string;
+  /**
+   * if not exists, it's falsy ""
+   */
+  public get getCurElId(): string {
+    return this.curElId;
+  }
+
+  private curElClass: string;
+  /**
+   * if not exists, it's falsy ""
+   */
+  public get getCurElClass(): string {
+    return this.curElClass;
+  }
 
   constructor(
     root: string,
-    templateId: string,
+    template: string,
     insertWhere: InsertPosition,
     newTemplateClassName?: string
   ) {
     // 1. set up the anchor tag and the template tag
-    this.templateEl = document.getElementById(
-      templateId
-    )! as HTMLTemplateElement;
+    this.templateEl = document.getElementById(template)! as HTMLTemplateElement;
 
     if (!this.templateEl) {
-      throw new Error(`template ID ${templateId} is invalid!`);
+      throw new Error(`template ID or Class ${template} is invalid!`);
     }
 
     this.rootEl = document.querySelector(root)! as HTMLDivElement;
 
     if (!this.rootEl) {
-      throw new Error(`anchor ID ${root} is invalid!`);
+      throw new Error(`anchor ID or Class ${root} is invalid!`);
     }
 
     // 2. import a node from the template
@@ -38,11 +63,19 @@ export class TemplateInjector<TCreateEl extends HTMLElement> {
       this.createdEl.classList.add(newTemplateClassName);
     }
 
-    // 4. forward the position to insert the createdEl
+    // 4. save id or class name of the created element.
+    this.curElId = this.createdEl.id ?? "";
+    this.curElClass = this.createdEl.className ?? "";
+
+    // 5. forward the position to insert the createdEl
     this.insertAt(insertWhere);
   }
 
   private insertAt(where: InsertPosition): void {
     this.rootEl.insertAdjacentElement(where, this.createdEl);
+  }
+
+  public removeMyself(): void {
+    this.createdEl.remove();
   }
 }
